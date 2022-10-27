5ffdf48188154780f061dc48610492c5e0a3b74a
charles6919 <57742687+charles6919@users.noreply.github.com>
2022-10-27 18:17:28 +0900

    로딩 씬 기본 구현

---
+++ Assets/Scripts/Loading/SceneLoader.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour 
+{
+    public static int NextScene;
+
+    [SerializeField]
+    Image _progressBar;
+
+    private void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+
+    public static void LoadScene(int index)
+    {
+        NextScene = index;
+        SceneManager.LoadScene(0);
+    }
+
+    public static void LoadScene(string sceneName)
+    {
+        NextScene = SceneManager.GetSceneByName(sceneName).buildIndex;
+        SceneManager.LoadScene(0);
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+        AsyncOperation op;
+        op = SceneManager.LoadSceneAsync(NextScene);
+        op.allowSceneActivation = false;
+        float timer = 0f;
+
+        while(!op.isDone)
+        {
+            yield return null;
+            timer += Time.deltaTime;
+
+            if(op.progress < 0.9f)
+            {
+                _progressBar.fillAmount = Mathf.Lerp(_progressBar.fillAmount, op.progress, timer * 500f);
+                if(_progressBar.fillAmount >= op.progress)
+                {
+                    timer = 0f;
+                }
+            }
+            else
+            {
+                _progressBar.fillAmount = Mathf.Lerp(_progressBar.fillAmount, 1f, timer );
+                if(_progressBar.fillAmount >= 1f)
+                {
+                    op.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+        }
+    }
+
+
+}
